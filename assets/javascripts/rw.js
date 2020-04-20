9ea0dcdac696b75974c8d33c33cb09f46569a86c
Jason Lutz <jason.m.lutz@gmail.com>
2020-04-19 21:51:06 -0700

    minor
    Signed-off-by: Jason Lutz <jason.m.lutz@gmail.com>>

--- assets/javascripts/rw.js
+++ assets/javascripts/rw.js
@@ -1,9 +1,9 @@
-let startingPointInput = document.querySelector("input[id='starting-point-input']");
-let startingPointLog = document.getElementById('starting-point-log');
-
-startingPointInput.oninput = handleInput;
-
-function handleInput(e) {
-  startingPointLog.textContent = `Preparing to search for
-      ${e.target.value}.....`;
-}
+// let startingPointInput = document.querySelector("input[id='starting-point-input']");
+// let startingPointLog = document.getElementById('starting-point-log');
+//
+// startingPointInput.oninput = handleInput;
+//
+// function handleInput(e) {
+//   startingPointLog.textContent = `Preparing to search for
+//       ${e.target.value}.....`;
+// }
