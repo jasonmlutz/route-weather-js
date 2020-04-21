a0f2e17987b16aa0bfe2d583c31ed513a299f800
Jason Lutz <jason.m.lutz@gmail.com>
2020-04-21 16:50:18 -0700

    include string -> url, fetch mapbox data, build array of location data

--- assets/javascripts/rw.js
+++ assets/javascripts/rw.js
@@ -1,9 +1,70 @@
 // Goal: Build the necessary features, one step at a time.
 // 1. html form/input to get user starting location; display input string
-document.getElementById('origin-input-button').onclick = function logSearchInitiation() {
-  // is this function notation "standard'? should I declare variables for this?
+document.getElementById('origin-input-button').onclick = function logSearchInitiation(e) {
+  // note: e has no inpact on the function at this time.
+  let originInput = document.getElementById('origin-input').value; // originInput in window element *ask*
   document.getElementById('origin-log').textContent =
-    `Preparing to search for ${document.getElementById('origin-input').value}.....`;
+    originInput.length > 0
+      ? `Searching for ${originInput} .....`
+      : "Please enter a value!";
+      // TODO make this error handling more robust and organized
+      // TODO loop in fetchLocationSearch here
+  fetchLocationSearch(originInput)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      window.tempLog = data; // TODO remove this
+    })
 }
 
-// 2. implement encodeURI() for inputs like "spokane, wa"
+// 2. mapbox tools + fetch
+let baseRequestURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
+
+const generateMapboxURL = (searchText) => {
+  if (typeof searchText !== "string") {
+    console.warn(`searchText {${searchText}} is not of type "string"`);
+  } else {
+    encodedSearchText = encodeURI(searchText);
+    return `${baseRequestURL}${encodedSearchText}.json?access_token=${mapBoxToken}`;
+  }
+}
+
+const fetchLocationSearch = (searchText) => {
+  return fetch(generateMapboxURL(searchText))
+}
+
+// 3. parse and prepare to display the fetched place_names
+// basic structure:
+// tempLog.features.forEach(feature => {
+//     console.log(feature.place_name);
+// })
+
+// build a single object associated with one feature fetched from Mapbox
+const constructLocationObject = (inputFeature) => {
+  let outputObject = {
+    name: {
+      short: inputFeature.text,
+      long: inputFeature.place_name
+    },
+    coords: {
+      lat: inputFeature.center[1],
+      long: inputFeature.center[0]
+    },
+    id: inputFeature.id
+  };
+  return outputObject
+}
+
+// build an array of objects with place_name and other data
+const buildLocationArray = (mapboxData) => {
+  let len = mapboxData.features.length;
+  let locationArray = [];
+  for (let i = 0; i < len; i++) {
+    locationArray.push(
+      constructLocationObject(mapboxData.features[i])
+    )
+  }
+  return locationArray
+}
