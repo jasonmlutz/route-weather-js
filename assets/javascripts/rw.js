02352c495c35d369b3f628148434c070f532286c
Jason Lutz <jason.m.lutz@gmail.com>
2020-04-21 11:37:28 -0700

    html form + js script to get user starting location

--- assets/javascripts/rw.js
+++ assets/javascripts/rw.js
@@ -11,3 +11,8 @@
 // }
 
 // Goal: Build the necessary features, one step at a time.
+// [ ] 1. html form/input to get user starting location
+document.getElementById('origin-input-button').onclick = function logSearchInitiation(e) {
+  // is this function notation "standard'? should I declare variables for this?
+  document.getElementById('origin-log').textContent = `Preparing to search for ${e.target.value}.....`;
+}
